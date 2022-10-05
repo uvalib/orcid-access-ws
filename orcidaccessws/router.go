d83eccbe86bc41f9a16b0de016bd61461fd115c9
Dave Goldstein <dpg3k@virginia.edu>
2022-10-05 12:13:28 -0400

    Remove Prometheus support; should have done this a long time ago

--- orcidaccessws/router.go
+++ orcidaccessws/router.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/uvalib/orcid-access-ws/orcidaccessws/handlers"
 	"net/http"
 )
@@ -90,21 +88,19 @@ var routes = routeSlice{
 	},
 }
 
-//
 // NewRouter -- build and return the router
-//
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 
 	// add the route for the prometheus metrics
-	router.Handle("/metrics", HandlerLogger(promhttp.Handler(), "promhttp.Handler"))
+	//router.Handle("/metrics", HandlerLogger(promhttp.Handler(), "promhttp.Handler"))
 
 	for _, route := range routes {
 
 		var handler http.Handler = route.HandlerFunc
 		handler = HandlerLogger(handler, route.Name)
-		handler = prometheus.InstrumentHandler(route.Name, handler)
+		//handler = prometheus.InstrumentHandler(route.Name, handler)
 
 		router.
 			Methods(route.Method).
